a171fbe8318cd8ca1a0d8b5c39bf5345e8495934
Ramkrishn2701 <93088159+Ramkrishn2701@users.noreply.github.com>
2023-05-22 14:31:37 +0000

    made some changes

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
+name = 'krish'
+
 
 @app.route("/")
 def hello_world():
   return render_template("home.html")
 
-app.run(host="0.0.0.0",debug=True)
+
+app.run(host="0.0.0.0", debug=True)
