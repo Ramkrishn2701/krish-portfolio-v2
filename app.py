d4fe956a0699bf75835524bc21ec4545a192dd43
Ramkrishn2701 <93088159+Ramkrishn2701@users.noreply.github.com>
2023-05-22 16:50:30 +0000

    small changes

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-name = 'krish'
 
 
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html",
+                         name='Siva Rama Krishnan',
+                         position='Frontend Developer',
+                         type='fresher')
 
 
 app.run(host="0.0.0.0", debug=True)
